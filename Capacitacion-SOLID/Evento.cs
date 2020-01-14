a582e3e6c3c542a41d1733f64d5b3bab76debb04
ArielAguirre0420 <52249037+ArielAguirre0420@users.noreply.github.com>
2020-01-13 22:32:47 -0600

    Primera Version

---
+++ Capacitacion-SOLID/Evento.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capacitacion_SOLID
+{
+    public class Evento : Program
+    {
+        public string cNombreEvento { get; set; }
+
+        public string dtFechaEvento { get; set; }
+    }
+}
